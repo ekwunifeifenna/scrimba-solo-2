4b5af63fb42a32a124d5c178701b39098733eb42
ekwunifeifenna <ifennaekwunife73@gmail.com>
2023-11-01 20:55:21 -0700

    Add files Add a default data file and also a navbar file

---
+++ scrimba-solo-2/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGlobe } from "@fortawesome/free-solid-svg-icons"
+
+
+function Navbar(){
+    return(
+        <nav>
+
+           
+            <FontAwesomeIcon icon={faGlobe} size="2x" className="nav-icon" transform=" rignt-90"/>
+
+            <h1>Places I Want to Visit</h1>
+            
+            
+            
+        </nav>
+    )
+
+}
+
+export default Navbar
